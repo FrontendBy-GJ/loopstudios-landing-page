e3d7051fe79bd155278fd0a9921ca2a9263f23b2
Genaro Garcia <genaro.dev@outlook.com>
2023-06-10 08:56:47 -0500

    feat: add socials data for Footer component

---
+++ src/data/footerData.js
@@ -0,0 +1,25 @@
+import {
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+} from 'react-icons/ai';
+import { BsPinterest } from 'react-icons/bs';
+
+export const socials = [
+  {
+    Icon: AiFillFacebook,
+    id: 'facebook',
+  },
+  {
+    Icon: AiOutlineTwitter,
+    id: 'twitter',
+  },
+  {
+    Icon: BsPinterest,
+    id: 'pinterest',
+  },
+  {
+    Icon: AiOutlineInstagram,
+    id: 'instagram',
+  },
+];
